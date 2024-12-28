6a59e5d3d5c3a1f51dd4150c887c1dc68790d7e9
Ulrich Hornung <hornunguli@gmx.de>
2024-12-28 11:48:57 +0100

    export node type

--- lib/model/node.go
+++ lib/model/node.go
@@ -4,3 +4,16 @@ import "github.com/restic/restic/internal/restic"
 
 type ID = restic.ID
 type Node = restic.Node
+type NodeType = restic.NodeType
+
+var (
+	NodeTypeFile      = restic.NodeTypeFile
+	NodeTypeDir       = restic.NodeTypeDir
+	NodeTypeSymlink   = restic.NodeTypeSymlink
+	NodeTypeDev       = restic.NodeTypeDev
+	NodeTypeCharDev   = restic.NodeTypeCharDev
+	NodeTypeFifo      = restic.NodeTypeFifo
+	NodeTypeSocket    = restic.NodeTypeSocket
+	NodeTypeIrregular = restic.NodeTypeIrregular
+	NodeTypeInvalid   = restic.NodeTypeInvalid
+)
